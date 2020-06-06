75954d8c7897939bff4a8207f7a08b89617e62c8
jeffreyvanhelden <jeffrey.vanhelden@thewarehouse.co.nz>
2020-06-07 07:07:52 +1200

    es sink

--- src/main/java/guru/bonacci/timesup/source/SampleConsumer.java
+++ src/main/java/guru/bonacci/timesup/source/SampleConsumer.java
@@ -1,4 +1,6 @@
-package guru.bonacci.foo.reactive;
+package guru.bonacci.timesup.source;
+
+import static guru.bonacci.timesup.source.SampleProducer.TOPIC;
 
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -9,11 +11,11 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import guru.bonacci.foo.Movers.Mover;
+import guru.bonacci.timesup.Entities.Mover;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.kafka.receiver.KafkaReceiver;
@@ -21,24 +23,23 @@ import reactor.kafka.receiver.ReceiverOffset;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.receiver.ReceiverRecord;
 
-public class FooConsumer {
+public class SampleConsumer {
 
-    private static final Logger log = LoggerFactory.getLogger(FooConsumer.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(SampleConsumer.class.getName());
 
     private static final String BOOTSTRAP_SERVERS = "localhost:9092";
     private static final String SCHEMA_REGISTRY = "http://127.0.0.1:8081";
-    private static final String TOPIC = "mover";
 
-    private final ReceiverOptions<Integer, Mover> receiverOptions;
+    private final ReceiverOptions<String, Mover> receiverOptions;
     private final SimpleDateFormat dateFormat;
 
-    public FooConsumer(String bootstrapServers) {
+    public SampleConsumer(String bootstrapServers) {
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "sample-consumerss");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "sample-groupss");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "sample-consumerssss");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "sample-groupssss");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
       		  "io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer");
 		props.put("schema.registry.url", SCHEMA_REGISTRY);
@@ -49,13 +50,13 @@ public class FooConsumer {
 
     public Disposable consumeMessages(String topic, CountDownLatch latch) {
 
-        ReceiverOptions<Integer, Mover> options = receiverOptions.subscription(Collections.singleton(topic))
+        ReceiverOptions<String, Mover> options = receiverOptions.subscription(Collections.singleton(topic))
                 .addAssignListener(partitions -> log.debug("onPartitionsAssigned {}", partitions))
                 .addRevokeListener(partitions -> log.debug("onPartitionsRevoked {}", partitions));
-        Flux<ReceiverRecord<Integer, Mover>> kafkaFlux = KafkaReceiver.create(options).receive();
+        Flux<ReceiverRecord<String, Mover>> kafkaFlux = KafkaReceiver.create(options).receive();
         return kafkaFlux.subscribe(record -> {
-            ReceiverOffset offset = record.receiverOffset();
-            System.out.printf("Received message: topic-partition=%s offset=%d timestamp=%s key=%d value=%s\n",
+        	ReceiverOffset offset = record.receiverOffset();
+        	System.out.printf("Received message: topic-partition=%s offset=%d timestamp=%s key=%s value=%s\n",
                     offset.topicPartition(),
                     offset.offset(),
                     dateFormat.format(new Date(record.timestamp())),
@@ -69,7 +70,7 @@ public class FooConsumer {
     public static void main(String[] args) throws Exception {
         int count = 20;
         CountDownLatch latch = new CountDownLatch(count);
-        FooConsumer consumer = new FooConsumer(BOOTSTRAP_SERVERS);
+        SampleConsumer consumer = new SampleConsumer(BOOTSTRAP_SERVERS);
         Disposable disposable = consumer.consumeMessages(TOPIC, latch);
         latch.await(10, TimeUnit.SECONDS);
         disposable.dispose();
--- src/main/java/guru/bonacci/timesup/source/SampleProducer.java
+++ src/main/java/guru/bonacci/timesup/source/SampleProducer.java
@@ -1,55 +1,59 @@
-package guru.bonacci.foo.reactive;
+package guru.bonacci.timesup.source;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import guru.bonacci.foo.Movers.Mover;
+import guru.bonacci.timesup.Entities.Mover;
 import reactor.core.publisher.Flux;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 import reactor.kafka.sender.SenderRecord;
 
-public class FooProducer {
+public class SampleProducer {
 
-    private static final Logger log = LoggerFactory.getLogger(FooProducer.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(SampleProducer.class.getName());
 
     private static final String BOOTSTRAP_SERVERS = "localhost:9092";
     private static final String SCHEMA_REGISTRY = "http://127.0.0.1:8081";
-    private static final String TOPIC = "mover";
+    static final String TOPIC = "movers-t";
 
-    private final KafkaSender<Integer, Mover> sender;
+    private final KafkaSender<String, Mover> sender;
     private final SimpleDateFormat dateFormat;
 
-    public FooProducer(String bootstrapServers) {
+    public SampleProducer(String bootstrapServers) {
 
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "sample-producer");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
         		  "io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer");
 		props.put("schema.registry.url", SCHEMA_REGISTRY);
-		SenderOptions<Integer, Mover> senderOptions = SenderOptions.create(props);
+		SenderOptions<String, Mover> senderOptions = SenderOptions.create(props);
 
         sender = KafkaSender.create(senderOptions);
         dateFormat = new SimpleDateFormat("HH:mm:ss:SSS z dd MMM yyyy");
     }
 
     public void sendMessages(String topic, int count, CountDownLatch latch) throws InterruptedException {
-        sender.<Integer>send(Flux.range(1, count)
-                        .map(i -> SenderRecord.create(new ProducerRecord<>(topic, i, Mover.newBuilder().setId(i).setName("Message_" + i).build()), i)))
+        sender.send(Flux.range(1, count)
+                        .map(i -> { 
+                        	String id = UUID.randomUUID().toString();
+                        	return SenderRecord.create(new ProducerRecord<>(topic, id, Mover.newBuilder().setId(id).setLat(1.1f).setLon(3.11f).build()), i);
+                    		}))
               .doOnError(e -> log.error("Send failed", e))
               .subscribe(r -> {
                   RecordMetadata metadata = r.recordMetadata();
@@ -70,7 +74,7 @@ public class FooProducer {
     public static void main(String[] args) throws Exception {
         int count = 20;
         CountDownLatch latch = new CountDownLatch(count);
-        FooProducer producer = new FooProducer(BOOTSTRAP_SERVERS);
+        SampleProducer producer = new SampleProducer(BOOTSTRAP_SERVERS);
         producer.sendMessages(TOPIC, count, latch);
         latch.await(10, TimeUnit.SECONDS);
         producer.close();
