62275bff965ad221447b66f8597309cb37039e80
jeffreyvanhelden <jeffrey.vanhelden@thewarehouse.co.nz>
2020-07-10 20:04:57 +1200

    mover producer working trial

---
+++ track-app/src/main/java/guru/bonacci/timesup/track/MoverProducer.java
@@ -0,0 +1,28 @@
+package guru.bonacci.timesup.track;
+
+import java.util.concurrent.ExecutionException;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.confluent.ksql.api.client.Client;
+import io.confluent.ksql.api.client.ClientOptions;
+import io.confluent.ksql.api.client.KsqlObject;
+
+@ApplicationScoped
+public class MoverProducer {
+
+	static final String KSQLDB_SERVER_HOST = "ksqldb-server";
+	static final int KSQLDB_SERVER_HOST_PORT = 8088;
+
+	Client client;
+	
+	MoverProducer() {
+		ClientOptions options = ClientOptions.create().setHost(KSQLDB_SERVER_HOST).setPort(KSQLDB_SERVER_HOST_PORT);
+		client = Client.create(options);
+	}
+
+	void ping() throws InterruptedException, ExecutionException {
+		KsqlObject row = new KsqlObject().put("ID", "moverid").put("LAT", 0.90).put("LON", 0.90);
+		client.insertInto("MOVER", row).get();
+	}
+}
