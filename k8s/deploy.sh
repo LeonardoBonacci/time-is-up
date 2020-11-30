18029639b6d1bbd5d904c94894dc58e132ba97cb
leonardobonacci <aabcehmu@mailfence.com>
2020-11-30 17:22:15 +1300

    prometheus

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -48,9 +48,16 @@ kubectl wait --for=condition=ready pod my-cluster-kafka-0 --timeout=120s
 kubectl -n $NAMESPACE apply -f k8s/topics.yaml
 kubectl -n $NAMESPACE apply -f k8s/internal-topics.yaml
 
---------------------
+kubectl exec -i my-cluster-kafka-0 -c kafka -- \
+    bin/kafka-configs.sh --alter \
+    --bootstrap-server localhost:9092 \
+    --entity-type topics \
+    --entity-name track \
+    --add-config delete.retention.ms=60000
+
+####################
 kubectl create configmap kafka-config --from-literal=kafka.bootstrap.servers=my-cluster-kafka-bootstrap:9092 --from-literal=quarkus.kafka-streams.bootstrap-servers=my-cluster-kafka-bootstrap:9092
---------------------
+####################
 mvn clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true
 mvn clean package -Dquarkus.profile=docker-registry -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true
 mvn clean package -Dquarkus.container-image.push=true
@@ -62,7 +69,20 @@ kubectl apply -f target/kubernetes/kubernetes.yml
 #2020-11-24 23:44:03,650 INFO  [io.quarkus] (main) react-unmoved-gate 1.0-SNAPSHOT native (powered by Quarkus 1.9.2.Final) started in 0.027s. Listening on: http://0.0.0.0:8080
 docker exec -it kind-control-plane crictl images
 
-
 kubectl get service my-cluster-kafka-external-bootstrap -o=jsonpath='{.spec.ports[0].nodePort}{"\n"}' # 32100
 kubectl get node kind-control-plane -o=jsonpath='{range .status.addresses[*]}{.type}{"\t"}{.address}{"\n"}' # 172.18.0.2
 kubectl exec my-cluster-kafka-0 -c kafka -it -- cat /tmp/strimzi.properties | grep advertised
+
+################## INSTALL PROMETHEUS AND MORE FOR KUBE CLUSTER MONITORING
+https://www.replex.io/blog/kubernetes-in-production-the-ultimate-guide-to-monitoring-resource-metrics
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install --name prometheus prometheus-community
+
+kubectl create namespace monitoring
+helm install --generate-name prometheus-community/prometheus-operator --namespace monitoring
+
+kubectl port-forward -n monitoring prometheus-prometheus-operator-160669-prometheus-0 9090:9090
+
+sum(rate(container_cpu_usage_seconds_total{container_name!="POD",pod!=""}[5m]))
+rate(container_cpu_usage_seconds_total{container_name!="POD",pod!=""}[5m])
--- react-unmoved-gate/src/main/java/guru/bonacci/timesup/runmoved/UnmovedResource.java
+++ react-unmoved-gate/src/main/java/guru/bonacci/timesup/runmoved/UnmovedResource.java
@@ -11,6 +11,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
@@ -22,28 +25,31 @@ import lombok.extern.slf4j.Slf4j;
 @Path("/unmoved")
 public class UnmovedResource {
 
-	@Inject @Channel("unmoved-channel") Emitter<Unmoved> emitter;
+	@Inject
+	@Channel("unmoved-channel")
+	Emitter<Unmoved> emitter;
 
-	
-	@POST //http POST localhost:30000/unmoved < src/main/resources/payload.json
+	@POST // http POST localhost:30000/unmoved < src/main/resources/payload.json
 	@Consumes(APPLICATION_JSON)
-    public void add(@Valid Unmoved unmoved) {
+	@Counted(name = "unmovedAdditions", description = "How many unmoved's have been added.")
+	@Timed(name = "unmovedTimer", description = "A measure of how long it takes to perform the add an unmoved.", unit = MetricUnits.MILLISECONDS)
+	public void add(@Valid Unmoved unmoved) {
 		log.info("add {}", unmoved);
 		emitter.send(KafkaRecord.of(unmoved.id, unmoved));
 	}
-	
-	@DELETE //http DELETE localhost:30000/unmoved/{id}
+
+	@DELETE // http DELETE localhost:30000/unmoved/{id}
 	@Path("/{id}")
-    public void remove(@PathParam(value = "id") String id) {
+	public void remove(@PathParam(value = "id") String id) {
 		log.info("remove Unmoved {}", id);
 		emitter.send(KafkaRecord.of(id, null));
 	}
-	
+
 	@POST
 	@Path("/echo")
-    @Produces(APPLICATION_JSON)
+	@Produces(APPLICATION_JSON)
 	@Consumes(APPLICATION_JSON)
-    public String echo(String requestBody) {
-        return requestBody;
-    }
+	public String echo(String requestBody) {
+		return requestBody;
+	}
 }
